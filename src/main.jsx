d01821eb99bbe3a04fdae666deff07ad2c9600bd
lyle pelayo <lylepelayo@gmail.com>
2025-04-21 22:58:21 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,11 @@ import "bootstrap/dist/js/bootstrap.js";
 import axios from "axios";
 axios.defaults.withCredentials = true;
 
-export const backend_server = "https://back-end-c25l.onrender.com";
+export const backend_server = "https://back-end-99ek.onrender.com";
 
 // Set up axios headers to help with CORS
 axios.defaults.headers.common["Access-Control-Allow-Origin"] =
-  "front-end-123.vercel.app";
+  "https://front-end-123.vercel.app";
 axios.defaults.headers.common["Content-Type"] = "application/json";
 
 // Wrap App in a loading handler component
